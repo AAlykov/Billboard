a5a21e833b6743c4563a882d18bb8e25dd25b3c1
Alexey Alykov <aalykov@gmail.com>
2022-08-01 20:25:12 +0700

    First Commit

---
+++ app/src/main/java/com/tomsk/alykov/billboard/act/EditAdsAct.kt
@@ -0,0 +1,30 @@
+package com.tomsk.alykov.billboard.act
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.tomsk.alykov.billboard.R
+import com.tomsk.alykov.billboard.databinding.ActivityEditAdsBinding
+import com.tomsk.alykov.billboard.utils.CityHelper
+
+class EditAdsAct : AppCompatActivity() {
+
+
+    private lateinit var rootElement: ActivityEditAdsBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        rootElement = ActivityEditAdsBinding.inflate(layoutInflater)
+        val view = rootElement.root
+        setContentView(view)
+        //setContentView(R.layout.activity_edit_ads)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, CityHelper.getAllCountries(this))
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        rootElement.spCountry.adapter = adapter
+
+
+    }
+}
