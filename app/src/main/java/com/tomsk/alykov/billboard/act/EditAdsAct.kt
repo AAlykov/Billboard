16814941fd4c8baa9966bff995759311c9b3031b
AlykovAB <alykov_ab@mail.ru>
2022-08-08 16:37:29 +0700

    Added alert dialog for choosing country

--- app/src/main/java/com/tomsk/alykov/billboard/act/EditAdsAct.kt
+++ app/src/main/java/com/tomsk/alykov/billboard/act/EditAdsAct.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.tomsk.alykov.billboard.R
 import com.tomsk.alykov.billboard.databinding.ActivityEditAdsBinding
+import com.tomsk.alykov.billboard.dialogs.DialogSpinnerHelper
 import com.tomsk.alykov.billboard.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
@@ -20,6 +21,11 @@ class EditAdsAct : AppCompatActivity() {
         setContentView(view)
         //setContentView(R.layout.activity_edit_ads)
 
+        val listCountry = CityHelper.getAllCountries(this)
+        val dialog = DialogSpinnerHelper()
+        dialog.showSpinnerDialog(this, listCountry)
+
+        /*
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, CityHelper.getAllCountries(this))
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         rootElement.spCountry.adapter = adapter
@@ -27,7 +33,7 @@ class EditAdsAct : AppCompatActivity() {
         val cityNames = CityHelper.getAllCountries(this)
         val spinner = rootElement.spinner
         val adapter2 = ArrayAdapter(this,android.R.layout.simple_spinner_item, cityNames)
-        spinner.adapter = adapter2
+        spinner.adapter = adapter2 */
 
 
     }
--- app/src/main/java/com/tomsk/alykov/billboard/utils/CityHelper.kt
+++ app/src/main/java/com/tomsk/alykov/billboard/utils/CityHelper.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import org.json.JSONObject
 import java.io.IOException
 import java.io.InputStream
+import java.util.*
+import kotlin.collections.ArrayList
 
 object CityHelper {
     fun getAllCountries(context: Context):ArrayList<String> {
@@ -32,4 +34,20 @@ object CityHelper {
         }
         return tempArray
     }
+
+    fun filterListData( list: ArrayList<String>, searchText: String?): ArrayList<String> {
+        val tempList = ArrayList<String>()
+        tempList.clear()
+        for (selection: String in list) {
+            if (searchText != null) {
+                if (selection.toLowerCase(Locale.ROOT).startsWith(searchText.toLowerCase(Locale.ROOT))) {
+                    tempList.add(selection)
+                }
+            }
+        }
+        if (tempList.size == 0)
+            tempList.add("No result")
+        return tempList
+    }
+
 }
